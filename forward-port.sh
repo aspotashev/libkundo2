9bd111cbfa907e9cbee9b7ddeaf088c4ef293037
Alexander Potashev <aspotashev@gmail.com>
2011-05-13 00:57:13 +0400

    update forward-port.sh

--- forward-port.sh
+++ forward-port.sh
@@ -30,23 +30,26 @@ do
 #	sed -i "s/QUndoCommand\b/KUndoCommand2/g" "$fn"
 #	sed -i "s/KUndoStack\b/KUndoStack2/g" "$fn"
 
-	sed_i "s/#include <QUndoView>/#include <kundoview2.h>/g" "$fn"
-	sed_i "s/#include <QUndoCommand>/#include <kundocommand2.h>/g" "$fn"
-	sed_i "s/#include <QtGui\/QUndoCommand>/#include <kundocommand2.h>/g" "$fn"
-	sed_i "s/#include <kundostack.h>/#include <kundostack2.h>/g" "$fn"
-	sed_i "s/#include <KUndoStack>/#include <kundostack2.h>/g" "$fn"
-	sed_i "s/#include <QUndoStack>/#include <kundostack2.h>/g" "$fn"
-	sed_i "s/#include <QUndoGroup>/#include <kundogroup2.h>/g" "$fn"
-	sed_i "s/#include \"qundostack\.h\"/#include <kundostack2.h>/g" "$fn"
-	sed_i "s/#include \"qundogroup\.h\"/#include <kundogroup2.h>/g" "$fn"
+#-----------------
+	sed_i "s/#include <QUndoView>/#include <kundo2view.h>/g" "$fn"
 
-	# temporary
-	sed_i "s/#include <QtGui\/KUndoCommand2>/#include <kundocommand2.h>/g" "$fn"
+	sed_i "s/#include <QUndoCommand>/#include <kundo2command.h>/g" "$fn"
+	sed_i "s/#include <QtGui\/QUndoCommand>/#include <kundo2command.h>/g" "$fn"
 
-	sed_i "s/QUndoView\b/KUndoView2/g" "$fn"
-	sed_i "s/QUndoCommand\b/KUndoCommand2/g" "$fn"
-	sed_i "s/KUndoStack\b/KUndoStack2/g" "$fn"
-	sed_i "s/QUndoStack\b/KUndoStack2/g" "$fn"
-	sed_i "s/QUndoGroup\b/KUndoGroup2/g" "$fn"
+	sed_i "s/#include <KUndoStack>/#include <kundo2stack.h>/g" "$fn"
+	sed_i "s/#include <kundostack.h>/#include <kundo2stack.h>/g" "$fn"
+
+	sed_i "s/#include <QUndoStack>/#include <kundo2qstack.h>/g" "$fn"
+	sed_i "s/#include \"qundostack\.h\"/#include <kundo2qstack.h>/g" "$fn"
+
+	sed_i "s/#include <QUndoGroup>/#include <kundo2group.h>/g" "$fn"
+	sed_i "s/#include \"qundogroup\.h\"/#include <kundo2group.h>/g" "$fn"
+#-----------------
+
+	sed_i "s/QUndoView\b/KUndo2View/g" "$fn"
+	sed_i "s/QUndoCommand\b/KUndo2Command/g" "$fn"
+	sed_i "s/KUndoStack\b/KUndo2Stack/g" "$fn"
+	sed_i "s/QUndoStack\b/KUndo2QStack/g" "$fn"
+	sed_i "s/QUndoGroup\b/KUndo2Group/g" "$fn"
 done
 
